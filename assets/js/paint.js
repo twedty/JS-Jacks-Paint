8e2551ece08bfbbe401827477125157fe1c6d3c4
twedty <jamestwedt@gmail.com>
2024-03-19 20:01:32 -0400

    looked at solution to code to see what I was missing with the dim. Found that I didn't have the appropriate css as well as an if statement because when I tried to just add/remove any opacity it would be immediate without the hover

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -3,21 +3,26 @@ function configureListeners() {
     // select img elements  
 
 
-     for (var i = 0; i < images.length; i++) {     
-        images[i].addEventListener('mouseover', addOpacity);
-        images[i].addEventListener('mouseout', removeOpacity);
+    for (const i = 0; i < images.length; i++) {     
+        images[i].addEventListener('mouseover', addOpacity, false);
+        images[i].addEventListener('mouseout', removeOpacity, false);
         // iterate over images and add mouseover event listeners      
     } 
 }
 
 function addOpacity(event) {
     // add appropriate CSS class
-    event.target.classList.add('opacity');
+    if (!this.classList.contains('dim')){
+        this.classList.add('dim')
+    }
     getProductInfo(event.target.id);     
 }
 
 function removeOpacity(event) {
      //remove appropriate CSS class
+    if (this.classList.contains('dim')){
+        this.classList.remove('dim')
+    }
     event.target.classList.remove('opacity');
 
     let element = document.getElementById('color-price');
@@ -37,44 +42,51 @@ function getProductInfo(paintColor) {
         case 'pn1':
             colorName = 'Lime Green';
             price = '$14.99';
-            removeOpacity(paintColor)
+            updatePrice(colorName, price)
             break;           
         case 'pn2':
             colorName = 'Medium Brown';
             price = '$11.14';
+            updatePrice(colorName, price)
             break;            
         case 'pn3':
             colorName = 'Royal Blue';
             price = '$22.99';
+            updatePrice(colorName, price)
             break;   
         case 'pn4':
             colorName = 'Solid Red';
             price = '$13.42';
+            updatePrice(colorName, price)
             break;   
         case 'pn5':
             colorName = 'Solid White';
             price = '$21.98';
+            updatePrice(colorName, price)
             break;
         case 'pn6':
             colorName = 'Solid Black';
             price = '$4.99';
+            updatePrice(colorName, price)
             break;   
         case 'pn7':
             colorName = 'Solid Cyan';
             price = '$8.22';
+            updatePrice(colorName, price)
             break; 
         case 'pn8':
             colorName = 'Solid Purple';
             price = '$11.99';
+            updatePrice(colorName, price)
             break;   
         case 'pn9':
             colorName = 'Solid Yellow';
             price = '$14.99';
+            updatePrice(colorName, price)
             break;
         default:
             break;             
     }
-    updatePrice(colorName, price);
 }
 function updatePrice(colorName, price) {
     let colorPrice = document.getElementById('color-price');
