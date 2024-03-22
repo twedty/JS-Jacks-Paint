c05cc3378cef2aa0bc95b23c77359acdc4d4359c
twedty <jamestwedt@gmail.com>
2024-03-21 21:15:31 -0400

    showing an error

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,7 +1,7 @@
 function configureListeners() {
     const images = document.getElementsByTagName('img')
 
-    for (let i = 0; i < images.length; i++) {     
+    for (const i = 0; i < images.length; i++) {     
         document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false);
         document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false);
     } 
